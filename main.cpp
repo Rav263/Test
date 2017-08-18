41de847a25a24f3fc8078260c917e604ea1b131d
Rav263 <nickiforov.nik@gmail.com>
2017-08-18 09:57:00 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-
+#include <string>
 
 int main(){
   std::cout << "Hello world!" << std::endl;
+  std::cout << "Please enter your name: ";
 
+  std::string name;
+  std::cin >> name;
+
+  std::cout << "Hello, " << name << std::endl;
+
+  std::cout << "How are you?" << std::endl;
 }
